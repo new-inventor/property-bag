853e5db7420be740017fda4287d84c595e44be80
George Ionov <g.ionov@php73.ru>
2017-09-27 20:38:14 +0400

    now enum normalizer accept callable at first parameter

--- src/Normalizer/EnumNormalizer.php
+++ src/Normalizer/EnumNormalizer.php
@@ -9,6 +9,7 @@ namespace NewInventor\PropertyBag\Normalizer;
 
 
 use NewInventor\PropertyBag\Exception\NormalizeException;
+use NewInventor\TypeChecker\TypeChecker;
 
 class EnumNormalizer extends AbstractNormalizer
 {
@@ -20,11 +21,17 @@ class EnumNormalizer extends AbstractNormalizer
     /**
      * EnumNormalizer constructor.
      *
-     * @param array               $availableValues
+     * @param array|callable      $availableValues
      * @param NormalizerInterface $normalizer
+     *
+     * @throws \NewInventor\TypeChecker\Exception\TypeException
      */
-    public function __construct(array $availableValues, NormalizerInterface $normalizer = null)
+    public function __construct($availableValues, NormalizerInterface $normalizer = null)
     {
+        TypeChecker::check($availableValues)-> tarray()->tcallable()->fail();
+        if(is_callable($availableValues)){
+            $availableValues = $availableValues();
+        }
         $this->availableValues = array_flip(array_unique($availableValues));
         $this->normalizer = $normalizer;
     }
