c264bb8aa66e59347d4a9bc4236321d62d22a1ef
George Ionov <g.ionov@php73.ru>
2017-09-28 16:59:06 +0400

    new camel case normalizer

---
+++ src/Normalizer/CamelCaseNormalizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Project: property-bag
+ * User: george
+ * Date: 28.09.17
+ */
+
+namespace NewInventor\PropertyBag\Normalizer;
+
+
+class CamelCaseNormalizer extends StringNormalizer
+{
+    protected function normalizeInputValue($value)
+    {
+        $value = parent::normalizeInputValue($value);
+        $value = preg_replace_callback(
+            '/[^a-zA-Z0-9](.)/',
+            function ($matches) {
+                return strtoupper($matches[1]);
+            },
+            $value
+        );
+        
+        return preg_replace('/[^a-zA-Z0-9]/', '', $value);
+    }
+}
